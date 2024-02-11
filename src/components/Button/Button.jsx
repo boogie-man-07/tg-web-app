8a00ed9208909586820c893e64c4ec0bb353d032
boogie-man-07 <booogie.man.07@gmail.com>
2024-02-11 17:43:05 +0300

    Initial commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
     return(
-        <button {...props} className={'Button_' + props.className} />
+        <button {...props} className={'button ' + props.className} />
     );
 };
 
