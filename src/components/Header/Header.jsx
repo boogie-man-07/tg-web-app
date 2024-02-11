f85f5bd7e09c4c99ebdc56aa687082207c74c509
boogie-man-07 <booogie.man.07@gmail.com>
2024-02-11 15:43:48 +0300

    Initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
+import './Header.css';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
@@ -15,4 +16,4 @@ const Header = () => {
     );
 };
 
-export default header;
+export default Header;
