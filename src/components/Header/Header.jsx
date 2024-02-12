af870e143050cb35e444e3880cd6985cfab81190
boogie-man-07 <booogie.man.07@gmail.com>
2024-02-12 18:13:13 +0300

    Initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,6 @@ const Header = () => {
     return(
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
         </div>
     );
 };
